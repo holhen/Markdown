babb6734de7f167b50d25ed581f862f796ba0efc
holhen <holhen@gmail.com>
2019-09-09 15:25:09 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import myTextFile from "./text.txt"
+import marked from 'marked'
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      text: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    fetch(myTextFile).then(response => response.text())
+                     .then(data => this.setState({
+                       text: data
+                     }))
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  getMarkdownText() {
+    //const marked = require("marked");
+    marked.setOptions({
+      breaks: true
+    });
+    var rawMarkup = marked(this.state.text, {sanitize: true});
+    return { __html: rawMarkup };
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor-header"><h3>Editor</h3></div>
+      <textarea id="editor" value={this.state.text} onChange={this.handleChange}></textarea>
+      <div className="preview-header"><h3>Preview</h3></div>
+      <div id="preview" dangerouslySetInnerHTML={this.getMarkdownText()}></div>
     </div>
   );
+  }
 }
 
 export default App;
